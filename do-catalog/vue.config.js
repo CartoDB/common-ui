e2b506c4d73bc5220f8d91b5f65c7d9ad004ae4a
Raúl Yeguas <neokore@gmail.com>
2020-05-19 14:59:22 +0200

    Use minified version and include images

--- do-catalog/vue.config.js
+++ do-catalog/vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   lintOnSave: false,
-  css: { extract: false }
+  css: { extract: false },
+  chainWebpack: config => {
+    config.module.rule('images')
+      .use('url-loader')
+      .loader('url-loader')
+      .options({ limit: 5120000 })
+  }
 };
