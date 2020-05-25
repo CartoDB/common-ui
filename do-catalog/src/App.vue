0d0cc99382afe38ef66fd9f6dfc13609c9c57e95
alberto <albert.a.g@gmail.com>
2020-05-25 16:22:21 +0200

    Disabled lint to prevent warnings

--- do-catalog/src/App.vue
+++ do-catalog/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" class="do-root">
+  <div id="app">
     <router-view :key="$route.fullPath"></router-view>
   </div>
 </template>
@@ -16,7 +16,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  ::v-deep {
-    @import './styles/main.scss';
-  }
+/* stylelint-disable */
+::v-deep {
+  @import './styles/main.scss';
+}
+/* stylelint-enable */
 </style>
