e43aa2bede87ce18ec4e72b2686639ad3b211e76
Raúl Yeguas <neokore@gmail.com>
2020-05-29 18:37:51 +0200

    Refactor DOCatalog initialization into a new class and allow inyecting data and methods [ch69098]

--- do-catalog/vue.config.js
+++ do-catalog/vue.config.js
@@ -6,5 +6,14 @@ module.exports = {
       .use('url-loader')
       .loader('url-loader')
       .options({ limit: 5120000 })
+
+    config.module.rule('svg')
+      .uses
+      .delete('file-loader')
+
+    config.module.rule('svg')
+      .use()
+      .loader('url-loader')
+      .options({ limit: 51200 })
   }
 };
